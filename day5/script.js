7618569e2cdda60a659283544f464cd5891c3969
lostfromlight1 <kaungthanlwinkyaw@gmail.com>
2026-01-12 15:46:43 +0630

    added sticky nav and mobile nav bar and new script

---
+++ day5/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.getElementById("menuBtn");
+    const mobileMenu = document.getElementById("mobileMenu");
+
+    if (menuBtn && mobileMenu) {
+        menuBtn.addEventListener("click", () => {
+            mobileMenu.classList.toggle("active");
+        });
+    }
+});
